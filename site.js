7694d0fd1af35abf8a1077a30252f8a648910c5b
innosoltech <innovativesolgh@gmail.com>
2026-09-02 15:07:14 +0000

    update to site.js

--- site.js
+++ site.js
@@ -84,15 +84,15 @@ $(document).ready(function () {
     ],
     responsive: {
       0: {
-        items: 1,
+        items: 1.15,
       },
 
       768: {
-        items: 2,
+        items: 2.15,
         margin: 10,
       },
       1000: {
-        items: 3,
+        items: 3.15,
       },
     },
   });
